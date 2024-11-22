b3d5866aa550d3a1fbcfbc0fb77470920b50e7c7
elvesbd <elvesbd@yahoo.com>
2024-11-22 20:43:29 -0300

    test: this adding unit tests for ride service

---
+++ backend/src/test/__mocks__/data-builder/index.ts
@@ -0,0 +1,4 @@
+export * from './ride/ride-data-builder';
+export * from './route/route-databuilder';
+export * from './driver/driver-data-builder';
+export * from './geolocation/geolocation-databuilder';
