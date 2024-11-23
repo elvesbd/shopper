d23e55045e4ba0df6457cae259dc91a8fb7728df
elvesbd <elvesbd@yahoo.com>
2024-11-23 14:51:09 -0300

    chore: this adjusting parse driver id pipe

--- backend/src/application/adapters/pipes/parser-driver-id.pipe.ts
+++ backend/src/application/adapters/pipes/parser-driver-id.pipe.ts
@@ -1,16 +1,18 @@
-import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import { Injectable, PipeTransform } from '@nestjs/common';
 
 @Injectable()
 export class ParseDriverIdPipe implements PipeTransform {
-  transform(value: any) {
-    if (value === undefined || value === null) {
+  transform(value: any): number | undefined {
+    if (
+      value === undefined ||
+      value === null ||
+      value === '' ||
+      isNaN(Number(value))
+    ) {
       return undefined;
     }
 
     const parsedValue = parseInt(value, 10);
-    if (isNaN(parsedValue)) {
-      throw new BadRequestException('Driver ID must be a valid number');
-    }
 
     return parsedValue;
   }
