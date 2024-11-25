e5a12416fbda0ab1ca183f73d5dffb05136ea6d5
elvesbd <elvesbd@yahoo.com>
2024-11-25 17:42:28 -0300

    chore: this add logger

--- frontend/src/pages/Rides/Confirm/components/StaticMap/index.tsx
+++ frontend/src/pages/Rides/Confirm/components/StaticMap/index.tsx
@@ -5,12 +5,16 @@ function StaticMapWithRoute() {
 	const { encodedPolyline, origin, destination } = useConfirm();
 
 	const googleMapsApiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+	console.log('googleMapsApiKey', googleMapsApiKey);
+	
 	const encodedPath = encodeURIComponent(encodedPolyline);
 
 	const originMarker = `${origin.latitude},${origin.longitude}`;
 	const destinationMarker = `${destination.latitude},${destination.longitude}`;
 
 	const staticMapUrl = `https://maps.googleapis.com/maps/api/staticmap?size=800x200&path=enc:${encodedPath}&markers=color:blue|label:A|${originMarker}&markers=color:red|label:B|${destinationMarker}&key=${googleMapsApiKey}`;
+	console.log(staticMapUrl, staticMapUrl);
+	
 
 	return (
 		<MapContainer>
