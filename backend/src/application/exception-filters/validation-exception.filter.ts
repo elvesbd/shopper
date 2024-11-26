ebaa1f06b161459105112db6e670c695b27967c5
elvesbd <elvesbd@yahoo.com>
2024-11-26 19:51:25 -0300

    chore: this adds error filter

--- backend/src/application/exception-filters/validation-exception.filter.ts
+++ backend/src/application/exception-filters/validation-exception.filter.ts
@@ -1,23 +1,36 @@
 import {
   Catch,
-  ExceptionFilter,
   ArgumentsHost,
+  ExceptionFilter,
   BadRequestException,
 } from '@nestjs/common';
 import { Response } from 'express';
+
+interface CustomErrorResponse {
+  error_code?: string;
+  error_description?: string;
+  message?: string | string[];
+  statusCode?: number;
+}
 
 @Catch(BadRequestException)
 export class ValidationExceptionFilter implements ExceptionFilter {
   catch(exception: BadRequestException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
+    const responseBody = exception.getResponse() as CustomErrorResponse;
+    console.log(responseBody);
 
-    const responseBody = exception.getResponse();
+    if (responseBody.error_code) {
+      return response.status(400).json(responseBody);
+    }
 
     const formattedError = {
       error_code: 'INVALID_DATA',
-      error_description: responseBody['message'][0],
+      error_description: Array.isArray(responseBody.message)
+        ? responseBody.message[0]
+        : responseBody.message || 'Dados inválidos',
     };
 
-    return response.status(400).json(formattedError);
+    return response.status(responseBody.statusCode).json(formattedError);
   }
 }
